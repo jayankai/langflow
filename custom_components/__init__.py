a30884b2f404e6de3c51eaeea875c4d82b82d868
jayankai <jayanka@aisel.co>
2025-10-14 09:47:11 +0200

    intermediary commit 1

---
+++ custom_components/__init__.py
@@ -0,0 +1,2 @@
+# Custom Components for Langflow
+# This directory contains custom components that extend Langflow functionality
---
+++ init-langflow-db.sql
@@ -0,0 +1,2 @@
+-- Create langflow database if it doesn't exist
+CREATE DATABASE langflow OWNER db_admin;
